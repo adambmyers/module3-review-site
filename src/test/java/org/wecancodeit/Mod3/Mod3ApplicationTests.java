e2f6cf5b6fc3c21d30f16aa3386d48143b25a952
adambmyers <blase_adam@yahoo.com>
2019-02-19 10:54:05 -0500

    initial push

---
+++ src/test/java/org/wecancodeit/Mod3/Mod3ApplicationTests.java
@@ -0,0 +1,16 @@
+package org.wecancodeit.Mod3;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mod3ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
