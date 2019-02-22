666769b64433a55ec2a8a01a1ff510251e595ef0
jessicawright <wright.983@gmail.com>
2019-02-22 10:45:40 -0500

    fixed linking to signin page, added @Controller

--- src/main/java/org/wecancodeit/Mod3/controllers/HomeController.java
+++ src/main/java/org/wecancodeit/Mod3/controllers/HomeController.java
@@ -2,36 +2,39 @@ package org.wecancodeit.Mod3.controllers;
 
 import java.util.ArrayList;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.wecancodeit.Mod3.models.Park;
 import org.wecancodeit.Mod3.repositories.ParksRepositories;
 
+@Controller
+
 public class HomeController {
 
 	private ParksRepositories parks = new ParksRepositories(new ArrayList<Park>());
 	
 	@GetMapping("/")
-	public String home() {
+	public String index() {
 		return "index";
 	}
 	
 	@GetMapping("/signin")
 	public String signin() {
-		return "signin";
+		return "signin/signin";
 	}
 	
-	@GetMapping("/parks/add")
-	public String getParkForm (Model model) {
+	@GetMapping("/parks/addParks")
+	public String getParkForm(Model model) {
 		model.addAttribute("parks", parks.getParks());
-		return "parks/add";
+		return "parks/addParks";
 	}
 	
-	@PostMapping("/parks/add")
+	@PostMapping("/parks/addParks")
 	public String addPark(String name, String location, String type) {
 		parks.addPark(new Park(name, location, type));
-		return "redirect:/parks/" + name;
+		return "redirect:/parks/addParks";
 	}
 	
 	
