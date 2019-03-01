ec98647017447f1f3b6e72c60d1e369c8a726dfe
Ariel Webb <arielwebb12@gmail.com>
2019-03-01 14:17:18 -0500

    added location, description, and image to park

--- src/main/java/org/wecancodeit/Mod3/models/Park.java
+++ src/main/java/org/wecancodeit/Mod3/models/Park.java
@@ -6,10 +6,9 @@ import java.util.Collection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
-
-
 
 @Entity
 public class Park {
@@ -19,7 +18,11 @@ public class Park {
 	// id for each park is initialized here
 	private Long id;  
 	private String parkName;
-
+	private String location;
+	private String website;
+	@Lob
+	private String description;
+	private String parkImageUrl;
 	
 	@OneToMany(mappedBy="park")
 	private Collection<Review> reviews;
@@ -31,8 +34,12 @@ public class Park {
 		// don't put anything in here; it's a hook for JPA
 	}
 	
-	public Park(String parkName, Category ...categories) {
+	public Park(String parkName, String location, String website, String description, String parkImageUrl, Category ...categories) {
 		this.parkName = parkName;
+		this.location = location;
+		this.website = website;
+		this.description = description;
+		this.parkImageUrl = parkImageUrl;
 		this.categories = Arrays.asList(categories);
 	}
 
@@ -48,13 +55,32 @@ public class Park {
 		return parkName;
 	}
 	
+	public String getLocation() {
+		return location;
+	}
+	
+	public String getWebsite() {
+		return website;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public String getParkImageUrl() {
+		return parkImageUrl;
+	}
+	
 	public Collection<Review> getReviews(){
 		return reviews;
 	}
 
 	@Override
 	public String toString() {
-		return "Park [id=" + id + ", parkName=" + parkName + ", reviews=" + reviews + "]";
+		return "Park [id=" + id + ", parkName=" + parkName + ", location=" + location + ", description=" + description
+				+ ", parkImageUrl=" + parkImageUrl + ", reviews=" + reviews + ", categories=" + categories + "]";
 	}
+	
+	
 
 }
